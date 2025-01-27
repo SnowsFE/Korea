0df33637fbc8fa1146cc832fcf8dca200b57c06f
SnowsFE <snoerkr@gmail.com>
2025-01-27 23:17:59 +0900

    폴더구조설정

--- Client/src/components/Board/Pagination.jsx
+++ Client/src/components/Board/Pagination.jsx
@@ -1,1 +1,17 @@
 // 페이지네이션
+import React from "react";
+import styled from "styled-components";
+
+const Pagination = () => {
+  return (
+    <Section>
+      <div>Pagination</div>
+    </Section>
+  );
+};
+
+export default Pagination;
+
+const Section = styled.div`
+  width: 100%;
+`;
