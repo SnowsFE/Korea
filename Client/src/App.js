4fc52357bb90e249e9ba729ac8363cf06eeadfad
SnowsFE <snoerkr@gmail.com>
2025-01-28 00:52:44 +0900

    폴더구조설정

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Main from "./main/Korea";
+import Home from "./pages/Home";
+import BoardList from "./components/Board/BoardList";
+import PostDetail from "./components/Board/PostDetail";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/boards" element={<BoardList />} />
+        <Route path="/boards/:boardsId" element={<PostDetail />} />
       </Routes>
     </Router>
   );
--- Client/src/components/Board/BoardList.jsx
+++ Client/src/components/Board/BoardList.jsx
@@ -1,1 +1,17 @@
 // 게시판 목록 페이지
+import React from "react";
+import styled from "styled-components";
+
+const BoardList = () => {
+  return (
+    <Section>
+      <div>BoardList</div>
+    </Section>
+  );
+};
+
+export default BoardList;
+
+const Section = styled.div`
+  width: 100%;
+`;
