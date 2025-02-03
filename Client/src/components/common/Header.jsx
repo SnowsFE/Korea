13a0fd7e4865eb3ea974b6daf4fbaa63ad63b866
kwangche <dlrhkdcp11@gmail.com>
2025-02-03 15:58:31 +0900

    메인페이지작업

--- Client/src/components/common/Header.jsx
+++ Client/src/components/common/Header.jsx
@@ -82,7 +82,8 @@ const Section = styled.div`
 `;
 
 const Container = styled.div`
-  width: 1280px;
+  max-width: 1280px;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
