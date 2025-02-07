129b7c1676e8a1df2a8cc7da860da9b44440a6fa
SnowsFE <snoerkr@gmail.com>
2025-02-07 18:01:16 +0900

    이벤트페이지 추가

--- Client/src/components/common/Img/Image.jsx
+++ Client/src/components/common/Img/Image.jsx
@@ -1,15 +1,26 @@
 import styled from "styled-components";
 
 const Bg = () => {
-  <Bgimg>
-    <img src="/images/bg.jpg" alt="Bg"></img>;
-  </Bgimg>;
+  return (
+    <Bgimg>
+      <img src="/images/bg.jpg" alt="Bg" />
+    </Bgimg>
+  );
 };
 
 export default Bg;
 
 const Bgimg = styled.div`
+  position: relative;
+  overflow: hidden; /* 이미지가 부모 영역 밖으로 나가지 않도록 설정 */
+
   img {
-    width: 2000px;
+    width: 100%;
+    opacity: 0; /* 기본 상태에서 보이지 않음 */
+    transition: opacity 0.5s ease-in-out; /* 부드러운 전환 효과 */
+  }
+
+  &:hover img {
+    opacity: 1; /* 마우스가 도달했을 때 이미지 보이기 */
   }
 `;
