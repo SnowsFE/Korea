0ebd76e9330a851a5712f0d0400620c2a29dab3e
SnowsFE <snoerkr@gmail.com>
2025-02-14 14:14:06 +0900

    리액트 쿼리 적용

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home";
 import CommonLayout from "./components/common/Layout/CommonLayout";
 import Layout from "./components/common/Layout/Layout";
@@ -9,23 +10,28 @@ import Board from "./components/Board/Boards";
 import JobApi from "./pages/JobInfo/Job";
 import Test from "./pages/Test/TestInfo";
 
+// React Query 클라이언트 생성
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <CommonLayout />
-      <Routes>
-        <Route element={<Layout />}>
-          {/* Layout을 공통으로 적용 */}
-          <Route path="/" element={<Home />} />
-          <Route path="boards/*" element={<Board />} />
-          <Route path="notice/*" element={<Notice />} />
-          <Route path="job/*" element={<JobApi />} />
-          <Route path="test" element={<Test />} />
-        </Route>
-        <Route path="/event/*" element={<Event />} />
-        {/* Layout 없이 개별 렌더링 */}
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <CommonLayout />
+        <Routes>
+          <Route element={<Layout />}>
+            {/* Layout을 공통으로 적용 */}
+            <Route path="/" element={<Home />} />
+            <Route path="boards/*" element={<Board />} />
+            <Route path="notice/*" element={<Notice />} />
+            <Route path="job/*" element={<JobApi />} />
+            <Route path="test" element={<Test />} />
+          </Route>
+          <Route path="/event/*" element={<Event />} />
+          {/* Layout 없이 개별 렌더링 */}
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
